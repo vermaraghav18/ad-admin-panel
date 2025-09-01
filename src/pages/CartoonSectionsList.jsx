d32c381234238cbefa6ecdb82f1a87f244ccf0d7
raghaverma18 <raghav18verma@gmail.com>
2025-09-02 00:29:33 +0530

    Remove legacy cartoon pages and client models causing Vercel build error

--- src/pages/CartoonSectionsList.jsx
+++
@@ -1,7 +0,0 @@
-// src/pages/CartoonSectionEdit.jsx
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-export default function CartoonSectionEdit() {
-  return <Navigate to="/cartoon-hub" replace />;
-}
